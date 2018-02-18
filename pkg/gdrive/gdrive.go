ae062de64c274de2b0f44442228e0d655079072c
Keeley Erhardt <keeley@improbable.io>
2018-02-18 21:36:59 +0000

    Download actual text file contents from Google Drive

--- pkg/gdrive/gdrive.go
+++ pkg/gdrive/gdrive.go
@@ -1,31 +1,36 @@
 package gdrive
 
 import (
+	"bufio"
 	"context"
 	"net/http"
+	"strings"
 
 	pb "github.com/keelerh/omniscience/protos"
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/api/drive/v3"
 )
 
 type GoogleDriveService struct {
-	service *drive.Service
+	svc *drive.Service
 }
 
 func New(client *http.Client) (*GoogleDriveService, error) {
-	service, err := drive.New(client)
+	svc, err := drive.New(client)
 	if err != nil {
 		return nil, err
 	}
 
-	return &GoogleDriveService{service}, nil
+	return &GoogleDriveService{
+		svc: svc,
+	}, nil
 }
 
 func (g *GoogleDriveService) GetAll(ctx context.Context, in *pb.GetAllDocumentsRequest) (*pb.GetAllDocumentsResponse, error) {
 	var docs []*pb.Document
 	pageToken := ""
 	for {
-		q := g.service.Files.List()
+		q := g.svc.Files.List()
 		// If we have a pageToken set, apply it to the query
 		if pageToken != "" {
 			q = q.PageToken(pageToken)
@@ -37,14 +42,22 @@ func (g *GoogleDriveService) GetAll(ctx context.Context, in *pb.GetAllDocumentsR
 			}, err
 		}
 		for _, f := range r.Files {
+			// Only attempt to download text files
+			if !strings.HasPrefix(f.MimeType, "text") {
+				continue
+			}
+			words, err := downloadFile(g.svc, f.Id)
+			if err != nil {
+				log.Warningf("Unable to download file: FileId(%v)", f.Id)
+			}
+			// TODO: Only retrieve files modified after the last modified time specified in the request.
 			doc := pb.Document{
 				Id:          f.Id,
 				Name:        f.Name,
 				Description: f.Description,
 				Service:     pb.Service_GDRIVE,
 				Url:         f.WebViewLink,
-				// TODO: Retrieve actual content of file.
-				// TODO: Only retrieve files modified after the last modified time specified in the request.
+				Words:       words,
 			}
 			docs = append(docs, &doc)
 		}
@@ -57,4 +70,25 @@ func (g *GoogleDriveService) GetAll(ctx context.Context, in *pb.GetAllDocumentsR
 	return &pb.GetAllDocumentsResponse{
 		Documents: docs,
 	}, nil
+}
+
+func downloadFile(svc *drive.Service, fileId string) ([]string, error) {
+	resp, err := svc.Files.Get(fileId).Download()
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(resp.Body)
+	scanner.Split(bufio.ScanWords)
+	var words []string
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+
+	return words, nil
 }
